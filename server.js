f30c435b48537dd0ece8c24d1df35f116197d4cd
cgzapien <65615888+cgzapien@users.noreply.github.com>
2022-02-24 15:29:16 -0700

    add client file

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const cors = require("cors")
 const morgan = require("morgan")
 const port = process.env.PORT || 4000
 
-
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({ extended: true }))
