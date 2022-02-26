34cb755ac3d562999a0fa3bc39d8603ac6a62775
Diana I <89826035+dibarra721@users.noreply.github.com>
2022-02-25 17:46:49 -0700

    about us

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./Components/CheckoutForm";
 import Confirmation from "./Pages/Confirmation"
 import Navbar from "./Components/Navbar";
+import About from "./Pages/About";
 import "./app.css"
 const stripePromise = loadStripe("pk_test_51KF1JjEAAoEQmAmLZ4JLFRx2GCI3uNpw8TZGfvwIMCJg87cKEfsKRZJOtMNwGo5HpfjxPWWi5lX6N7i4LgP6Wgnx00SUKtRqJH")
 
@@ -12,10 +13,11 @@ export default function App(){
   return (
     <div >
       <Navbar />
+      <About/>
       {/* <Elements stripe={stripePromise} >
         <CheckoutForm  />
       </Elements> */}
-      <Confirmation />
+      {/* <Confirmation /> */}
 
     </div>
   )
---
+++ client/src/Pages/About.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import cockpit from "../images/PiperCockpit.png"
+export default function About () {
+
+return (
+
+<div className='aboutSection'>
+
+    <img src={cockpit} alt="plane"></img>
+<h1>1975 PIPER ARROW ||</h1>
+<center>
+<p> This beautiful 1975 Piper Arrow II 200 hp retractable complex plane. 
+    You can rent this plane out for business from instructing flight lessons or clocking your pilot 
+    hours day or night. You can also take the plane out for pleasure and enjoy a weekend up in Jackson Hole or a
+     night down in St. George; sky's the limit.</p><br/>
+   
+     $175 PER HOUR
+ 
+          RESERVE NOW
+ <br/>
+
+        *50 to 100 hour blocks are discounted | Overnight flights available 1
+        week in advance<br/>
+     </center>
+</div>
+)
+
+}
