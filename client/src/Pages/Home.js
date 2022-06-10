ad1b7445bfc032fe8eca57498b1e0d05f18d2142
Kevin Babauta <kjr.babauta@gmail.com>
2022-06-10 11:19:21 -0700

    homepage CSS, trying to get background image to display

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-import piper from "../media/PiperArrowIIRightSide.jpg"
+import "../CSS/homePage.css"
 
 export default function Home(){
     return (
-        <div className="homePage">
-            <h1>O-D Plane Rental</h1>
-            <p>This beautiful 1975 Piper Arrow II 200 hp retractable complex plane. You can rent this plane out for business from instructing flight lessons or clocking your pilot hours day or night. You can also take the plane out for pleasure and enjoy a weekend up in Jackson Hole or a night down in St. George; sky's the limit.</p>
+        <div className="home-page">
+            <div className="home-image"></div>
+                <div className="home-container">
+                    <h1>O-D Plane Rental</h1>
+                    <p>This beautiful 1975 Piper Arrow II 200 hp retractable complex plane. You can rent this plane out for business from instructing flight lessons or clocking your pilot hours day or night. You can also take the plane out for pleasure and enjoy a weekend up in Jackson Hole or a night down in St. George; sky's the limit.</p>
+                </div>
         </div>
     )
 }
